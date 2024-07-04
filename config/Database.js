007a85a1c70dfd735235e2e6a497865dfbb308a2
19333794 <abimanyu_bhamakerti_x@telkomsel.co.id>
2024-07-04 11:31:31 +0700

    update config db

--- config/Database.js
+++ config/Database.js
@@ -1,17 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-// const db = new Sequelize(process.env.MYSQL_DB,process.env.DB_USER,process.env.DB_PASS,{
-//     host: process.env.DB_HOST,
-//     dialect: 'mysql'
-// });
 
-// export default db;
-// const db = new Sequelize('ganffest','root','',{
-//     host: 'api.ganeshafilmfestival.com',
-//     dialect: 'mysql'
-// });
-
-// export default db;
 const db = new Sequelize(process.env.MYSQL_DB, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: 'mysql'
